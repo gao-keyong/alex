458c8206edd468a04308a31c35129d8e1c421df9
Gao Keyong <gaokeyong@outlook.com>
2021-10-06 17:55:35 +0800

    test for alex

---
+++ test/test1.c
@@ -0,0 +1,13 @@
+// Line comment
+#include <stdio.h>
+
+int main()
+{
+    /*
+     * Block comment
+     */
+    char *msg = "Hello ";
+    float d = 0.145e+3;
+    printf("%s %f\n", msg, d);
+    return 0;
+}
