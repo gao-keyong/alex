a045a338a9a99adf971d9e8aa7f3bc229c00c62b
Gao Keyong <gaokeyong@outlook.com>
2021-10-07 22:49:45 +0800

    split dec and def

--- lexer.cpp
+++ lexer.cpp
@@ -42,6 +42,275 @@ Lexer::~Lexer()
 {
 }
 
+void Lexer::getNextIdentifier(Token &token)
+{
+    token.setType("Identifier");
+    eatChar();
+    do
+    {
+        char ch = peekChar();
+        if (isalphadigit_(ch))
+        {
+            eatChar();
+        }
+        else
+            break;
+    } while (true);
+// 特判该token是否为关键词Keyword
+#define KEYWORD(TOK)     \
+if (buffer_ == #TOK) \
+    token.setType("Keyword"), count_Keyword++;
+#include "def/KEYWORD.def"
+#undef KEYWORD
+    if (token.getType() == "Identifier")
+    {
+        count_Identifier++;
+    }
+    token.setToken(buffer_);
+}
+
+void Lexer::getNextNumerical(Token &token)
+{
+    token.setType("Numerical_Constant");
+    eatChar();
+    // 识别数值常量的自动机，对应的状态转移图为Graph_1.jpg
+    unsigned state = 2;
+    char ch;
+    do
+    {
+        switch (state)
+        {
+        case 0:
+            break;
+        case 1:
+            ch = peekChar();
+            if (isalpha_(ch))
+            {
+                state = 1;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+        case 2:
+            ch = peekChar();
+            if (isdigit(ch))
+            {
+                state = 2;
+                eatChar();
+            }
+            else if (ch == '.')
+            {
+                state = 3;
+                eatChar();
+            }
+            else if (ise(ch))
+            {
+                state = 5;
+                eatChar();
+            }
+            else if (isalpha_(ch))
+            {
+                token.setType("Error");
+                token.setError("illegal name");
+                count_Error++;
+                state = 1;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        case 3:
+            ch = peekChar();
+            if (isdigit(ch))
+            {
+                state = 4;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        case 4:
+            ch = peekChar();
+            if (isdigit(ch))
+            {
+                state = 4;
+                eatChar();
+            }
+            else if (ise(ch))
+            {
+                state = 5;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        case 5:
+            ch = peekChar();
+            if (isdigit(ch))
+            {
+                state = 7;
+                eatChar();
+            }
+            else if (issign(ch))
+            {
+                state = 6;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        case 6:
+            ch = peekChar();
+            if (isdigit(ch))
+            {
+                state = 7;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        case 7:
+            ch = peekChar();
+            if (isdigit(ch))
+            {
+                state = 7;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        default:
+            break;
+        }
+    } while (state != 0);
+    token.setToken(buffer_);
+    if (token.getType() == "Numerical_Constant")
+    {
+        count_Numerical_Constant++;
+    }
+}
+
+void Lexer::getCharConstant(Token &token)
+{
+    token.setType("Char_Constant");
+    eatChar();
+    // 识别字符常量的自动机
+    unsigned state = 1;
+    char ch;
+    do
+    {
+        switch (state)
+        {
+        case 1:
+            ch = peekChar();
+            if (ch == '\\')
+            {
+                state = 3;
+                eatChar();
+            }
+            else if (ch == '\'')
+            {
+                state = 0;
+                eatChar();
+            }
+            else
+            {
+                state = 2;
+                eatChar();
+            }
+            break;
+        case 2:
+            ch = peekChar();
+            if (ch == '\'')
+            {
+                state = 0;
+                eatChar();
+            }
+            else
+            {
+                state = 0;
+            }
+            break;
+        case 3:
+            state = 2;
+            eatChar();
+            break;
+        default:
+            break;
+        }
+    } while (state != 0);
+    token.setToken(buffer_);
+    if (token.getType() == "Char_Constant")
+    {
+        count_Char_Constant++;
+    }
+}
+
+void Lexer::getStringLiteral(Token &token)
+{
+    token.setType("String_Literal");
+    eatChar();
+    // 识别字符串常量的自动机
+    unsigned state = 1;
+    char ch;
+    do
+    {
+        switch (state)
+        {
+        case 1:
+            ch = peekChar();
+            if (ch == '\\')
+            {
+                state = 2;
+                eatChar();
+            }
+            else if (ch == '\"')
+            {
+                state = 0;
+                eatChar();
+            }
+            else if (ch == '\n')
+            {
+                token.setType("Error");
+                token.setError("unclosed string");
+                count_Error++;
+                state = 0;
+            }
+            else
+            {
+                state = 1;
+                eatChar();
+            }
+            break;
+        case 2:
+            state = 1;
+            eatChar();
+            break;
+        default:
+            break;
+        }
+    } while (state != 0);
+    token.setToken(buffer_);
+    if (token.getType() == "String_Literal")
+    {
+        count_String_Literal++;
+    }
+}
+
 bool Lexer::lexer()
 {
     bool res = true;
